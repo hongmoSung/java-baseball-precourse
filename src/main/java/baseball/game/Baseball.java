df2ec344b0431293d15ee8d45287708cdeaa5ff8
hongmoSung <tesung30@naver.com>
2022-04-16 20:43:29 +0900

    feat: 베이스볼 게임 구현
    - 게임 사용자 화면
    - 재시작 여부
    - 점수 계산

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -3,6 +3,7 @@ package baseball.game;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -18,11 +19,34 @@ public class Baseball {
             System.out.print(integer);
         }
         System.out.println("\n");
-        System.out.print("숫자를 입력해 주세요:");
-        String inputValue = Console.readLine();
-        boolean b = checkEnteredValue(inputValue);
-        if (!b) throw new IllegalArgumentException();
-        System.out.println(inputValue);
+        userInterface(answer);
+    }
+
+    /**
+     * 사용자 인터페이스
+     * @param answer 컴퓨터 3자리 숫자
+     */
+    private void userInterface(List<Integer> answer) {
+        while (true) {
+            System.out.print("숫자를 입력해 주세요:");
+            String userInputValue = Console.readLine();
+            List<Integer> integers = checkEnteredValue(userInputValue);
+            if (!isCorrect(answer, integers)) continue;
+            if (isRetry()) this.gameStart();
+            System.out.println("게임이 종료 되었습니다.");
+            break;
+        }
+    }
+
+    /**
+     * 게임 재시작 여부
+     */
+    private boolean isRetry() {
+        String reg = "[1-2]$";
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userEnteredValue = Console.readLine().trim();
+        if (!Pattern.matches(reg, userEnteredValue)) throw new IllegalArgumentException();
+        return userEnteredValue.equals("1");
     }
 
     /**
@@ -41,8 +65,56 @@ public class Baseball {
      * @param enteredValue 사용자가 입력한 값
      * @return boolean
      */
-    private boolean checkEnteredValue(String enteredValue) {
+    private List<Integer> checkEnteredValue(String enteredValue) {
         String reg = "\\d{3}";
-        return Pattern.matches(reg, enteredValue.trim());
+        if (!Pattern.matches(reg, enteredValue.trim())) throw new IllegalArgumentException();
+        String trim = enteredValue.trim();
+        return Arrays.asList(
+                Integer.parseInt(String.valueOf(trim.charAt(0))),
+                Integer.parseInt(String.valueOf(trim.charAt(1))),
+                Integer.parseInt(String.valueOf(trim.charAt(2))));
     }
+
+    /**
+     * 컴퓨터 값과 사용자 값을 비교하여 결과를 출력
+     * @param computer 컴퓨터 값
+     * @param user 사용자 값
+     */
+    private boolean isCorrect(List<Integer> computer, List<Integer> user) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            for (int j = 0; j < user.size(); j++) {
+                if (computer.get(i).equals(user.get(j))) {
+                    if (i == j) {
+                        strike++;
+                        continue;
+                    }
+                    ball++;
+                }
+            }
+        }
+        return score(strike, ball);
+    }
+
+    /**
+     * 점수를 계산
+     * @param strike 스트라이크
+     * @param ball 볼
+     * @return boolean
+     */
+    private boolean score(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return false;
+        }
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+            return true;
+        }
+        System.out.println(strike + " 스트라이크 " + ball + "볼입니다.");
+        return false;
+    }
+
+
 }
